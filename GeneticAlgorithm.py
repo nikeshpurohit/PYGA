c9dcc41a3f5fdf8d0aec5f43701cf379fe8d4ba6
nikeshpurohit <hello@nikeshpuroh.it>
2019-11-04 20:33:59 +0000

    more progress

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,43 +1,57 @@
 import random
 
 N = 10 #number of bits in the string
-P = 10 #population size
+P = 10 #population size (no of individuals in the population)
 nGen = 3 #number of generations
 
 class individual():
     gene = [] 
     fitness = 0
-    def __init__(self):
-        print("indiviual created")
-        self.gene = gene
-        self.fitness = fitness
-        
 
+    def __init__(self):
+        self.gene = []
+        self.fitness = 0
+        
     def randomiseGene(self):
         for i in range (0, N):
             c = random.randrange(0, 2, 1)
             self.gene.append(c)
-            print(self.gene)
+            #print(self.gene)
 
-#class population():
-#    def __init__(self):
-#        pop = []
+    def printGene(self):
+         print(self.gene)
+        
+    def printFitness(self):
+        print(self.fitness)
 
-pop = [individual] #create the object P number of times
+population = [] #list to store the population of individuals
 
-def calculateFitness(indiv):
+def createInitalPopulation(): #create P number of individuals and append them to the list.
+    for x in range(0, P):
+        i = individual()
+        population.append(i)
+
+def randomisePopulation(): #function to set up the intial random genes in the population
+    for i in population:
+        i.randomiseGene()
+        i.printGene()
+
+def calculateFitness(indiv): #function to calculate and store the fitness value for a given individual
     count = 0
     for i in indiv.gene:
         if i == 1:
             count = count + 1
-    idiv.fitness = count
-    print(count)
+    indiv.fitness = count
 
+#initalise and randomise
+createInitalPopulation()
+randomisePopulation()
 
-def startingPop():
-    for i in range (0,1):
-        individual.gene.append(i)
+#generate fitness values
+for i in population: #calculate the fitness of each individual in population
+    calculateFitness(i)
+    #i.printFitness()
 
-i1 = individual
-i1.randomiseGene(i1)
-calculateFitness(i1)
+#selection
+winners = [] #a list to store the selected individuals
+
